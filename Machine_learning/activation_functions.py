9a3ca1169c9a939bf8ba13636442193ee72ee470
Jaume Palau <jpalauserrano@gmail.com>
2026-02-04 13:07:40 +0100

    Añado apuntes en neural_network y activation_functions

---
+++ Machine_learning/activation_functions.py
@@ -0,0 +1,68 @@
+''' EN ESTE ARCHIVO ESTAN LAS DISTINTAS FUNCIONES DE ACTIVACION QUE SE UTILIZAN'''
+import numpy as np
+
+def step_func(inputs):
+
+    '''Devuelve una salida binaria segun un umbral
+    Sirve para decisiones simples; no es diferenciable.
+    '''
+
+    output = []
+
+    for i in inputs:
+        if i > 0:
+            output.append(1)
+        else:
+            output.append(0)
+
+    return output
+
+
+def lineal_func(inputs): # y = m*x + b
+
+    '''Transforma la entrada con una recta
+    No introduce no-linealidad; varias funciones lineales seguidas equivalen a una sola.
+    '''
+
+    m = 1 # pendiente
+    b = 0 # sesgo
+
+    output = []
+
+    for x in inputs:
+        y = m*x +b
+        output.append(y)
+    
+    return output
+
+
+def sigmoide_func(inputs): # r = 1 / (1+np.e**(-x))
+
+    '''Comprime los valores en un rango entre 0 y 1
+    Se usa para interpretar salidas como probabilidades.
+    '''
+
+    output = []
+
+    for x in inputs: 
+        r = 1 / (1+np.e**(-x))
+        output.append(r)
+
+    return output
+
+
+def ReLu_func(inputs):
+
+    '''Función de activación que devuelve el valor de entrada si es positivo y 0 si es negativo;
+    introduce no linealidad y evita saturación en valores positivos.
+    '''
+
+    output = []
+
+    for i in inputs:
+        if i > 0:
+            output.append(i)
+        else:
+            output.append(0)
+
+    return output
--- Machine_learning/neural_network.py
+++ Machine_learning/neural_network.py
@@ -1,12 +1,11 @@
+'''COMO CONECTAR LAS CAPAS ENTRE SÍ'''
 
-## Clase 2 min 35
 import numpy as np
-
 
 class Capa_densa:
     # Inicializacion de la capa
 
-    def __init__(self,n_neuronas,n_inputs):
+    def __init__(self,n_inputs,n_neuronas):
 
         # inicio los pesos aleatorios
         self.weights = 0.01 * np.random.rand(n_inputs,n_neuronas)
@@ -16,9 +15,40 @@ class Capa_densa:
 
 
     def forward(self, inputs):
-
+        # Calcula los outputs de la capa a traves del producto escalar
         self.output = np.dot(inputs,self.weights) + self.bias  
 
-capa1 = Capa_densa(4,3)
-print(capa1.weights)
-print(capa1.bias)
+
+# Creamos dos vectores
+# VECTOR X: lista de n=100 entre 0 y 2*pi 
+x = np.linspace(0, 2 * np.pi, num=100)
+# VECTOR Y: lista de cosenos del vector x
+y = np.cos(x)
+
+# Creamos un array con lso inputs:
+x = np.array(x)
+y = np.array(y)
+
+# Creamosuna matriz con nuestros datos:
+Data = np.vstack((x,y)).T #(100,2) (muestras,inputs*entrada)
+
+# las columnas del data deben coincidir con el input de la capa
+
+capa1 = Capa_densa(2,3)
+capa2 = Capa_densa(3,6)
+
+'''
+CONEXION ENTRE CAPAS:
+    A LA CAPA1 LE ENTRA EL DATA INICIAL, A LA CAPA 2 LE ENTRA EL OUTPUT DE LA CAPA1
+    Y LA CAPA 2 DA UN OUTPUT FINAL
+'''
+#Forward:   
+# data.shape | wheights.shape | bias.shape → capa1.shape
+#    (100×2) · (2×3) + (1×3) → (100×3)
+capa1.forward(Data) # 
+
+# capa1.shape | wheigths.shape | bias.shape → capa2.shape
+#   (100x3) · (3x6) + (1x6) → (100x6)
+capa2.forward(capa1.output)
+
+print(capa2.output)
